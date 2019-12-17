53626c238b9ba3d686053c652c4d736ea7bd34be
ozne2 <ozne2@172.19.155.231>
2019-12-17 11:23:55 +0100

    Creazione delle classi model.

--- src/Model/Docente.java
+++ src/Model/Docente.java
@@ -1,5 +1,46 @@
 package Model;
 
-public class Docente {
+import java.util.ArrayList;
+
+public class Docente extends Utente {
+
+	/**
+	 * @param nome
+	 * @param cognome
+	 * @param matricola
+	 * @param password
+	 * @param email
+	 */
+	public Docente(String nome, String cognome, String matricola, String password, String email) {
+		super(nome, cognome, matricola, password, email);
+		orariDisponibili = new ArrayList<Disponibilita>();
+		// TODO Auto-generated constructor stub
+	}
 	
+	
+	
+	
+	//var
+	
+	/**
+	 * @return the orariDisponibili
+	 */
+	public ArrayList<Disponibilita> getOrariDisponibili() {
+		return orariDisponibili;
+	}
+
+
+
+
+	/**
+	 * @param orariDisponibili the orariDisponibili to set
+	 */
+	public void setOrariDisponibili(ArrayList<Disponibilita> orariDisponibili) {
+		this.orariDisponibili = orariDisponibili;
+	}
+
+
+
+
+	private ArrayList<Disponibilita> orariDisponibili;
 }
